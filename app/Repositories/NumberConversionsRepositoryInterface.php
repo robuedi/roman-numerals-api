0064fd2ed2e55f98ae7e5fcb5fd6af376bc6d93e
Eduard Robu <edi_cristi3@yahoo.com>
2021-03-16 13:57:32 +0000

    interface updates

--- app/Repositories/NumberConversionsRepositoryInterface.php
+++ app/Repositories/NumberConversionsRepositoryInterface.php
@@ -2,6 +2,15 @@
 
 namespace App\Repositories;
 
+use App\Repositories\ParamObj\NrConversionsRepoIndex;
+
 interface NumberConversionsRepositoryInterface
 {
+    public function incrementForValue(int $value): void;
+
+    /**
+     * @param array $params - fields:array, sort_by:string, order_by:string
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index(NrConversionsRepoIndex $params, bool $paginate = false);
 }
